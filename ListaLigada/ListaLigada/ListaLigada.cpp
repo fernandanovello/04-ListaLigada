7eb1aef71439b65cce776076fde3248472160263
Fernanda Novello <fernandaarts19@gmail.com>
2022-09-10 21:49:47 -0300

    Lista Ligada - buscarElemento - excluirElemento

--- ListaLigada/ListaLigada/ListaLigada.cpp
+++ ListaLigada/ListaLigada/ListaLigada.cpp
@@ -73,7 +73,7 @@ void menu()
 void inicializar()
 {
 
-	// se a lista j· possuir elementos
+	// se a lista j√° possuir elementos
 	// libera a memoria ocupada
 	NO* aux = primeiro;
 	while (aux != NULL) {
@@ -145,18 +145,57 @@ void inserirElemento()
 
 void excluirElemento()
 {
+	int paraExcluir;
 	
+	cout << "Elemento que deseja excluir: ";
+	cin >> paraExcluir;
+
+	NO* posExcluir = posicaoElemento(paraExcluir);
+
+	if (posExcluir != NULL) {
+		if (posExcluir == primeiro) {
+			primeiro = primeiro->prox;
+			free(posExcluir);
+		}
+		else {
+			NO* aux = primeiro;
+			while (aux != NULL) {
+				if(aux->prox == posExcluir){
+					aux->prox = posExcluir->prox;
+					free(posExcluir);
+				}
+				aux = aux->prox;
+			}
+		}
+		cout << "ELEMENTO EXCLUIDO!" << endl;
+	}
+	else {
+		cout << "ELEMENTO NAO ENCONTRADO!" << endl;
+	}
 }
+
 
 void buscarElemento()
 {
-	
+	int busca;
+
+	cout << "Busca: ";
+	cin >> busca;
+
+	if (posicaoElemento(busca) != NULL)
+	{
+		cout << "ELEMENTO ENCONTRADO!" << endl;
+	}
+	else
+	{
+		cout << "ELEMENTO NAO ENCONTRADO!" << endl;
+	}
+
 }
 
 
-
 // retorna um ponteiro para o elemento buscado
-// ou NULL se o elemento n„o estiver na lista
+// ou NULL se o elemento n√£o estiver na lista
 NO* posicaoElemento(int numero)
 {
 	NO* aux = primeiro;
